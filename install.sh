5da339ee6663d80ab4a6d8f0984a5e793481038c
Dorbian <dorbian@gmail.com>
2024-12-16 14:02:24 +0100

    Pathing error

--- install.sh
+++ install.sh
@@ -12,10 +12,9 @@ if [ ! -d "~/.config/bigtree" ]; then
 fi
 # Download latest to destination
 git clone https://github.com/dorbian/thebigtree.git ~/.config/bigtree
-
 # Checking if service exists
-if [ ! -e ~/.config/systemd/bigtree.service ]; then
-    cp ~/.config/bigtree/service/bigtree.service ~/.config/systemd/bigtree.service
+if [ ! -e ~/.config/systemd/user/bigtree.service ]; then
+    cp ~/.config/bigtree/service/bigtree.service ~/.config/systemd/user/bigtree.service
     systemctl daemon-reload --user
     systemctl enable bigtree.service --user
     systemctl start bigtree.service --user
