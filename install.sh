f134de90a8917f3fe503c2b16003ccd3aefd7135
Dorbian <dorbian@gmail.com>
2024-12-16 16:04:47 +0100

    Installation update

--- install.sh
+++ install.sh
@@ -1,27 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # check if user systemd dir exists
-if [ ! -d "~/.config/systemd/user" ]; then
+
+if [ ! -f ~/.config/systemd/user/bigtreeupdate.service ]; then
+    echo -e "Creating systemd user directory"
     mkdir -p ~/.config/systemd/user
 fi
-
-if [ ! -d "~/.config/bigtree" ]; then
+if [ ! -f ~/.config/bigtree/thebigtree.py ]; then
+    echo -e "Creating bigtree directory"
     mkdir -p ~/.config/bigtree
     # Download latest to destination
     git clone https://github.com/dorbian/thebigtree.git ~/.config/bigtree
 fi
 # Check if update service exists
-if [ ! -e ~/.config/systemd/user/bigtreeupdate.service ]; then
+if [ ! -f ~/.config/systemd/user/bigtreeupdate.service ]; then
+    echo -e "Creating Update Service"
     cp ~/.config/bigtree/service/bigtreeupdate.service ~/.config/systemd/user/bigtreeupdate.service
     cp ~/.config/bigtree/service/bigtreeupdate.timer ~/.config/systemd/user/bigtreeupdate.timer
     systemctl daemon-reload --user
     systemctl enable bigtreeupdate.timer --user
 fi
 # Checking if service exists
-if [ ! -e ~/.config/systemd/user/bigtree.service ]; then
+if [ ! -f ~/.config/systemd/user/bigtree.service ]; then
+    echo -e "Creating service for BigTree Discord Bot"
     cp ~/.config/bigtree/service/bigtree.service ~/.config/systemd/user/bigtree.service
     systemctl daemon-reload --user
     systemctl enable bigtree.service --user
     systemctl start bigtree.service --user
 fi
-  systemctl restart bigtreeupdate.timer --user
+echo -e "Starting update service"
+systemctl restart bigtreeupdate.timer --user
