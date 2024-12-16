5892dc081ed640b4fc8ca936b4697bce8a9fd029
Dorbian <dorbian@gmail.com>
2024-12-16 15:20:23 +0100

    Update service update

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-curdir=${pwd}
-# allow user mode systemd over ssh
-loginctl --enable-linger
+
 # check if user systemd dir exists
 if [ ! -d "~/.config/systemd/user" ]; then
     mkdir -p ~/.config/systemd/user
@@ -12,6 +10,13 @@ if [ ! -d "~/.config/bigtree" ]; then
     # Download latest to destination
     git clone https://github.com/dorbian/thebigtree.git ~/.config/bigtree
 fi
+# Check if update service exists
+if [ ! -e ~/.config/systemd/user/bigtreeupdate.service ]; then
+    cp ~/.config/bigtree/service/bigtreeupdate.service ~/.config/systemd/user/bigtreeupdate.service
+    cp ~/.config/bigtree/service/bigtreeupdate.timer ~/.config/systemd/user/bigtreeupdate.timer
+    systemctl daemon-reload --user
+    systemctl enable bigtreeupdate.timer --user
+fi
 # Checking if service exists
 if [ ! -e ~/.config/systemd/user/bigtree.service ]; then
     cp ~/.config/bigtree/service/bigtree.service ~/.config/systemd/user/bigtree.service
@@ -19,6 +24,4 @@ if [ ! -e ~/.config/systemd/user/bigtree.service ]; then
     systemctl enable bigtree.service --user
     systemctl start bigtree.service --user
 fi
-# Copy cronjob if needed
-# cp checkscript
-# chmod checkscript chmod 755 hourly-event.sh
+  systemctl start bigtreeupdate.timer --user
--- service/update.sh
+++ service/update.sh
@@ -5,7 +5,9 @@ update_status=$(git log ..origin/main --oneline | wc -l)
 if (( update_status > 0 )); then
     systemctl stop bigtree --user
     git pull    
-    cp ~/.config/bigtree/service/bigtree.service ~/.config/systemd/user/bigtree.service
+    /bin/cp -rf ~/.config/bigtree/service/bigtree.service ~/.config/systemd/user/bigtree.service
+    /bin/cp -rf ~/.config/bigtree/service/bigtreeupdate.service ~/.config/systemd/user/bigtreeupdate.service
+    /bin/cp -rf ~/.config/bigtree/service/bigtreeupdate.timer ~/.config/systemd/user/bigtreeupdate.timer
     systemctl daemon-reload --user
     systemctl start bigtree --user
 fi
